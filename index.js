a113fdc1a50534bde609b5305c055e6752094f9e
Farhan Asif <f.asif2010@gmail.com>
2020-05-28 00:33:15 +0600

    database setup

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 // Create express app
 var express = require("express")
-var app = express()
+const bodyParser = require("body-parser");
+const app = express();
+const fs = require("fs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+var db = require("./database.js")
 
 // Server port
 var HTTP_PORT = 8000 
@@ -11,6 +28,21 @@ app.listen(HTTP_PORT, () => {
 // Root endpoint
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
+});
+
+app.get("/books", (req, res, next) => {
+    var sql = "select * from books"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
 });
 
 // Insert here other API endpoints
